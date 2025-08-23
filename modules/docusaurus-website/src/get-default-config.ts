c067c21c85f94ce8371b46ea5e8a7e18972ca693
Chris Gervang <chris@gervang.com>
2025-08-23 00:51:04 -0700

    lint

--- modules/docusaurus-website/src/get-default-config.ts
+++ modules/docusaurus-website/src/get-default-config.ts
@@ -221,7 +221,8 @@ export function getDocusaurusConfig(config: OcularWebsiteConfig): Config {
             {
               type: 'html',
               position: 'right',
-              value: '<a href="https://openvisualization.org" target="_blank" style="content: \'\'; height: 28px; width: 100px; background-image: url(\'/openjs-foundation.svg\'); background-repeat: no-repeat;  background-size: 100px 28px; display: flex">'
+              value:
+                '<a href="https://openvisualization.org" target="_blank" style="content: \'\'; height: 28px; width: 100px; background-image: url(\'/openjs-foundation.svg\'); background-repeat: no-repeat;  background-size: 100px 28px; display: flex">'
             },
             ...navbarItems
           ].filter(Boolean)
@@ -272,7 +273,8 @@ export function getDocusaurusConfig(config: OcularWebsiteConfig): Config {
               ]
             }
           ],
-          copyright: '<p>Copyright <a href="https://openjsf.org">OpenJS Foundation</a> and vis.gl contributors. All rights reserved. The <a href="https://openjsf.org">OpenJS Foundation</a> has registered trademarks and uses trademarks. For a list of trademarks of the <a href="https://openjsf.org">OpenJS Foundation</a>, please see our <a href="https://trademark-policy.openjsf.org">Trademark Policy</a> and <a href="https://trademark-list.openjsf.org">Trademark List</a>. Trademarks and logos not indicated on the <a href="https://trademark-list.openjsf.org">list of OpenJS Foundation trademarks</a> are trademarks&trade; or registered&reg; trademarks of their respective holders. Use of them does not imply any affiliation with or endorsement by them.</p><p><a href="https://openjsf.org">The OpenJS Foundation</a> | <a href="https://terms-of-use.openjsf.org">Terms of Use</a> | <a href="https://privacy-policy.openjsf.org">Privacy Policy</a> | <a href="https://bylaws.openjsf.org">Bylaws</a> | <a href="https://code-of-conduct.openjsf.org">Code of Conduct</a> | <a href="https://trademark-policy.openjsf.org">Trademark Policy</a> | <a href="https://trademark-list.openjsf.org">Trademark List</a> | <a href="https://www.linuxfoundation.org/cookies">Cookie Policy</a></p>'
+          copyright:
+            '<p>Copyright <a href="https://openjsf.org">OpenJS Foundation</a> and vis.gl contributors. All rights reserved. The <a href="https://openjsf.org">OpenJS Foundation</a> has registered trademarks and uses trademarks. For a list of trademarks of the <a href="https://openjsf.org">OpenJS Foundation</a>, please see our <a href="https://trademark-policy.openjsf.org">Trademark Policy</a> and <a href="https://trademark-list.openjsf.org">Trademark List</a>. Trademarks and logos not indicated on the <a href="https://trademark-list.openjsf.org">list of OpenJS Foundation trademarks</a> are trademarks&trade; or registered&reg; trademarks of their respective holders. Use of them does not imply any affiliation with or endorsement by them.</p><p><a href="https://openjsf.org">The OpenJS Foundation</a> | <a href="https://terms-of-use.openjsf.org">Terms of Use</a> | <a href="https://privacy-policy.openjsf.org">Privacy Policy</a> | <a href="https://bylaws.openjsf.org">Bylaws</a> | <a href="https://code-of-conduct.openjsf.org">Code of Conduct</a> | <a href="https://trademark-policy.openjsf.org">Trademark Policy</a> | <a href="https://trademark-list.openjsf.org">Trademark List</a> | <a href="https://www.linuxfoundation.org/cookies">Cookie Policy</a></p>'
         },
         algolia: typeof search === 'object' ? search : undefined,
         prism: {
